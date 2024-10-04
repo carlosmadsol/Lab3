5bd1fb7ae38f7c3bb5da80365c87072f334db023
Carlos <carlos.madrigalsolis@ucr.ac.cr>
2024-10-03 18:29:24 -0600

    Se modifico el script para que ahora se obtenga el factorial por un numero dado

--- Ejercicio_2.c
+++ Ejercicio_2.c
@@ -4,13 +4,15 @@ int factorial (int n) {
     int i = 1;
     while (n > 1) {
         i = i * n;
-        n = n - 1;
+        n = n - 1; //El error estaba en que se estaba definiendo una una n con int n, lo que hacia que el n del while nunca cambiara
     }
     return i ;
 }
 int main (int argc , char * argv [ ]) {
-    int fac4 = factorial (4);
-    int fac5 = factorial (5);
-    printf ( " 4! = %d , 5! = %d\n" , fac4, fac5);
+    int num; //Agregamos y definimos la variable num como un integrer
+    printf("Ingrese un número: "); //Agregamos este printf para que a la hora de ingresar el numero se sienta mas interactivo
+    scanf("%d", &num); //Agregamos el scanf que lo que hace es pedirle al usuario de la terminal que agregue un numero que sera asignado a num
+    int resultado_factorial = factorial (num); //Creamos esta variable resultado_factorial para guardar lo obtenido de la funcion factorial
+    printf("%d! = %d\n", num, resultado_factorial); 
     return 0;
 }
